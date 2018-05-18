a7d767cc7bc1d7a253f04f136e87ca99a78dc8a8
huangguofei <778445440@qq.com>
2018-05-18 21:21:33 +0800

    2018年5月18日21:20:58

--- src/app/HttpServer.ts
+++ src/app/HttpServer.ts
@@ -4,7 +4,7 @@ import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HttpServer {
-  HttpHead = {headers: new HttpHeaders({'Content-Type': 'application/json'})};
+  HttpHead = new HttpHeaders({'Content-Type': 'application/json'}).append('Cookie','Hm_lvt_c50daaaa5cfdae76b896d28564d58cf5=1526646998; Hm_lpvt_c50daaaa5cfdae76b896d28564d58cf5=1526646998; Hm_lvt_a80131c47c9510dc37c21b92f3404763=1526646998; Hm_lpvt_a80131c47c9510dc37c21b92f3404763=1526646998');
 
   constructor(private httpClient: HttpClient) {
 
@@ -13,7 +13,7 @@ export class HttpServer {
   // 统一发送请求
   public request(params: any): any {
     if (params['method'] === 'post' || params['method'] === 'POST') {
-      return this.post(params['utl'], params['data']);
+      return this.post(params['url'], params['data']);
     } else {
       return this.get(params['url'], params['data']);
     }
@@ -21,21 +21,24 @@ export class HttpServer {
 
   // get请求
   public get(url: string, params: any): any {
-    return this.httpClient.get(url, this.HttpHead)
+    return this.httpClient.get(url, {headers: this.HttpHead})
       .toPromise().then(this.handleSuccess)
       .catch(res => this.handleError(res));
   }
 
   // post请求
   public post(url: string, params: any) {
-    return this.httpClient.post(url, params, this.HttpHead)
-      .toPromise().then(this.handleSuccess)
-      .catch(res => this.handleError(res));
+    console.log(url)
+    return this.httpClient.post(url, params, {
+      headers: this.HttpHead
+   }).toPromise().then(this.handleSuccess).catch(res => this.handleError(res));
+    // .toPromise().then(this.handleSuccess)
+    // .catch(res => this.handleError(res));
   }
 
   // 成功请求
   public handleSuccess(res: any) {
-    let body = res['body'];
+    const body = res['body'];
     if (body) {
       return {
         data: res.json().data || {},
@@ -52,7 +55,7 @@ export class HttpServer {
   // 处理失败
   public handleError(error) {
     console.log(error);
-    let msg = '请求失败';
+    const msg = '请求失败';
     if (error.status === 400) {
       console.log('请求参数正确');
     }
--- src/app/server/TestServer.ts
+++ src/app/server/TestServer.ts
@@ -6,10 +6,15 @@ export class TestServer {
   constructor(private httpServer: HttpServer) {
 
   }
+
   test() {
+    $cookie.set('Hm_lpvt_c50daaaa5cfdae76b896d28564d58cf5', '1526646998');
+    $cookie.set('Hm_lvt_a80131c47c9510dc37c21b92f3404763', '1526646998');
+    $cookie.set('Hm_lpvt_a80131c47c9510dc37c21b92f3404763', '1526646998');
+
     return this.httpServer.request({
       method: 'post',
-      url: 'http://www.yuanyoudadi.com/yydd-web/product/list',
+      url: 'yydd-web/product/list',
       data: {}
     });
   }
